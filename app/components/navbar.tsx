13603a780a8e26f61e1dddf3b56b9d9b2b4a707d
Kruk43854 <pawel.kruk@studenci.collegiumwitelona.pl>
2026-03-02 15:10:02 +0100

    changed navigation

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -7,8 +7,8 @@ export default function Navbar() {
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
-    <nav className="bg-green-700 fixed w-full z-20 top-0 start-0 border-b border-green-800 shadow-md">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav className="bg-green-700 fixed w-full z-20 top-0 border-b border-green-800 shadow-md">
+      <div className="max-w-7xl flex flex-wrap items-center justify-between mx-auto p-4">
         
         <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
         {/* tu bedzie logo */}
@@ -21,7 +21,7 @@ export default function Navbar() {
         <button
           onClick={toggleMenu}
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-green-100 rounded-lg md:hidden hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-green-300"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-green-100 rounded-xl-lg md:hidden hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-green-300"
           aria-controls="navbar-default"
           aria-expanded={isMenuOpen}
         >
@@ -33,9 +33,9 @@ export default function Navbar() {
 
         <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-green-800 rounded-lg bg-green-700 md:flex-row md:space-x-2 md:mt-0 md:border-0">
-            <li><Link to="/" className="block py-2 px-4 text-white rounded hover:bg-green-800 transition-colors">Menu</Link></li>
-            <li><Link to="/about" className="block py-2 px-4 text-white rounded hover:bg-green-800 transition-colors">O nas</Link></li>
-            <li><Link to="/contact" className="block py-2 px-4 text-white rounded hover:bg-green-800 transition-colors">Kontakt</Link></li>
+            <li><Link to="/menu" className="block py-2 px-4 text-white rounded-2xl hover:bg-green-800 transition-colors">Menu</Link></li>
+            <li><Link to="/reservation" className="block py-2 px-4 text-white rounded-2xl hover:bg-green-800 transition-colors">Rezerwacja</Link></li>
+            <li><Link to="/about" className="block py-2 px-4 text-white rounded-2xl hover:bg-green-800 transition-colors">O nas</Link></li>
           </ul>
         </div>
       </div>
