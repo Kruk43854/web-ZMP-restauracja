c16ec29ffc8f25055e6067cad8b1886005529233
Kruk43854 <pawel.kruk@studenci.collegiumwitelona.pl>
2026-03-04 19:59:50 +0100

    added new routes

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,9 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("reservation", "routes/reservation.tsx"),
-    route("menu", "routes/menu.tsx")
+    route("menu", "routes/menu.tsx"),
+    route("about", "routes/about.tsx"),
+    route("login", "routes/login.tsx"),
+    route("register", "routes/register.tsx")
 
     ] satisfies RouteConfig;
