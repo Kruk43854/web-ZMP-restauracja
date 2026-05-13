a6cbe79d1e900e93aff2edeeaac5365a241c02f2
Kruk43854 <pawel.kruk@studenci.collegiumwitelona.pl>
2026-05-13 10:01:08 +0200

    test

--- app/routes/reservation.tsx
+++ app/routes/reservation.tsx
@@ -84,7 +84,7 @@ export default function Reservation() {
 
     const generateTimeSlots = () => {
     const slots = [];
-    for (let h = 12; h <= 22; h++) {
+    for (let h = 0; h <= 22; h++) {
         for (let m = 0; m < 60; m += 15) {
             slots.push(`${h.toString().padStart(2, '0')}:${m.toString().padStart(2, '0')}`);
         }
